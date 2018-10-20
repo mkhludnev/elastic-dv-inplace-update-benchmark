ca4aeb18bce55cfdfadc71336b646255a811da5b
Mikhail Khludnev <mkhludnev@ebsco.com>
2018-10-20 23:48:11 +0300

    class as param source

--- field_update/track.py
+++ field_update/track.py
@@ -93,6 +93,92 @@ def insert_books_with_subscription_closure(words):
         return output
     return insert_books
 
+class InsertBooksSubsParamSource:
+    def __init__(self, track, params, **kwargs):
+        self._index_name = params.get("index", "books")
+        self._type_name = params.get("type", "books")
+        self._params = params
+        self._bulk_size = int(params.get("bulk-size", "100"))
+        self._max_subs = int(params.get("subs-total", "10000"))
+        self._num_subs = int(params.get("subs-per-book", "100"))
+        self._max_books = int(params.get("books-total", "10000000"))
+        self._words=[]
+        try:
+             with open('/usr/share/dict/words', 'r') as f:
+                 for l in f :
+                    self._words.append(l.replace("\n",""))
+        except Exception as e:
+            self._words +=['foo','bar','baz','lorem','ipsum','dolores','moo','ban','crux','boom','greed','block']
+            print(e)
+    def partition(self, partition_index, total_partitions):
+        return InsertBooksSubsClient(self,partition_index,total_partitions)
+    #def size(self):
+    #    raise NotImplementedError("expected to run in partition")
+    #def params(self):
+    #    raise NotImplementedError("expected to run in partition")
+
+class InsertBooksSubsClient:
+    def __init__(self, factory:InsertBooksSubsParamSource, partition_index, total_partitions):
+        self._factory=factory
+        self.partition_index=partition_index
+        self.total_partitions=total_partitions
+        self.iter = self.create_iter()
+    def create_iter(self):
+        """
+        yield n-th bulk 
+        """
+        book_per_part=self._factory._max_books/self.total_partitions
+        i=0
+        body=""
+        for book_id in range(int(book_per_part*self.partition_index), int(book_per_part*(self.partition_index+1))):
+            subscs=[]
+            for s in range(0,self._factory._num_subs):
+               subscs.append(hex(random.randint(0,self._factory._max_subs)))
+            body+=(json.dumps({ "index" : {"_id" : book_id, "_type" : self._factory._type_name, "_index" : self._factory._index_name} })+'\n')
+            #{"title": "Book of minerals", "author": "Albertus, Magnus, Saint, 1193?-1280", "pubDate": "1967","subscriptions": ["CK","MA","AH"]}
+            body+=(json.dumps({  
+                "title": " ".join(self.random_text(4)),
+                "author": " ".join(self.random_text(2)),
+                "abstract": " ".join(self.random_text(100)),
+                "pubDate": 1837+(i+51)%(2018-1837),
+                "subscriptions": subscs})+'\n')
+            i+=1
+            if i%self._factory._bulk_size:
+                b=body
+                yield {
+                    "body":b,
+                    "action-metadata-present":True,
+                    "bulk-size":self._factory._bulk_size,
+                    "index":self._factory._index_name,
+                    "type":self._factory._type_name
+                }
+                body=""
+        if body!="":
+            b=body
+            yield {
+                "body":b,
+                "action-metadata-present":True,
+                "bulk-size":bulkSize,
+                "index":self._factory._index_name,
+                "type":self._factory._type_name
+            }
+            body=""
+        return
+    def size(self):
+        """"
+        number of bulks per partition
+        """
+        return self._factory._max_books/self.total_partitions/self._factory._bulk_size
+    def params(self):
+        return next(self.iter)
+
+    def random_text(self,num_words):
+        result=[]
+        for i in range(0,num_words):
+            result.append(random.choice(self._factory._words))
+        return result
+
+
 def register(registry):
     registry.register_param_source("search-param-source", get_random_search_parameters)
     registry.register_param_source("update-param-source", get_random_books_update_query)
@@ -105,12 +191,18 @@ def register(registry):
         words +=['foo','bar','baz','lorem','ipsum','dolores','moo','ban','crux','boom','greed','block']       
         print(e)
     registry.register_param_source("insert-books-subscription",insert_books_with_subscription_closure(words))
+    registry.register_param_source("insert-books-subs-parallel",InsertBooksSubsParamSource)
+
 
 def main():
     w=[]
     w+=["foo","bar"]
     out=str(insert_books_with_subscription_closure(w)({"a":1},{"b":2}))
     assert(out.count("foo")+out.count("bar")>0)
+    
+    c = InsertBooksSubsParamSource({"a":1},{"b":2})
+    part = c.partition(3, 4).params()
+    print(part)
     return
     
 if __name__ == "__main__":
