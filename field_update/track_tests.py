e59e7206a3968152287975acb09ccf9ea6c79f48
Mikhail Khludnev <mkhludnev@ebsco.com>
2018-10-22 21:40:21 +0300

    reduce concurrency

--- field_update/track_tests.py
+++ field_update/track_tests.py
@@ -1,12 +1,14 @@
 import unittest
+import json
 from track import InsertBooksSubsParamSource
 
 class TestBooksSubsInsert(unittest.TestCase):
 
     def test_math(self):
+        bulk_size=1000
         c = InsertBooksSubsParamSource({"a":1},
                                        {
-                        "bulk-size":1000,
+                        "bulk-size":bulk_size,
                         "subs-total":2000,
                         "subs-per-book":10,
                         "books-total": 10000000 
@@ -17,6 +19,8 @@ class TestBooksSubsInsert(unittest.TestCase):
           bulks = part.size()
           for b in range(0, bulks):
               params = part.params()
+              body=params["body"].split("\n")
+              assert len(body)==bulk_size*2+1
               if p==0 and b==0:
                    print(params)
         return
